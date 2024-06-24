2d26ac83cc333b9aa6edb6d12c57bc8b4443663f
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-06-24 18:30:25 +0600

    updates

---
+++ src/slick/script.js
@@ -0,0 +1,33 @@
+$(".popular_restaurant").slick({
+  infinite: false,
+  touchMove: true,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1040,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});

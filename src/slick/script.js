624fd6298ba64a4532beed8b55605ea801de9829
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-06-25 21:47:48 +0600

    updates

--- src/slick/script.js
+++ src/slick/script.js
@@ -31,3 +31,42 @@ $(".popular_restaurant").slick({
     // instead of a settings object
   ],
 });
+
+// customar review
+
+$(".customar_reviews").slick({
+  infinite: false,
+  touchMove: true,
+  arrows: true,
+  prevArrow:'.prevArrow',
+  nextArrow:'.nextArrow',
+  speed: 300,
+   
+  responsive: [
+    {
+      breakpoint: 1040,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
+
